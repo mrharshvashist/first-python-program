151f37f8c9edc582aaec9a17797a1b07d345cd8d
mrharshvashist <mrharshvashist@gmail.com>
2025-08-02 15:59:49 +0530

    Initialize Flask application and add basic HTML template File handling complete

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-
+# day 01
 # all_students = {
 #   "student_01" : {
 #       "name": "Alice",
@@ -25,6 +25,7 @@
 # input("Press Enter to continue...")
 # This line is used to pause the script execution
 
+# day 02
 
 # create a sample text file
 # f = open("students.txt", "a")
@@ -43,6 +44,8 @@
 
 # n = open("C:\\\\Users\\HARSH\\Downloads\\greetings.txt")
 
+# day 03
+
 # content = n.read()
 # print(content)
 # # The above code reads the content of a file named "greetings.txt" located in the
@@ -59,6 +62,8 @@
 # print(r.read())
 # with open("learning_file.txt", "a") as learning_file:
 #     learning_file.write("- deleting files with os module.\n")
+
+# day 04
 
 # import os
 # os.remove("new_file.txt")
@@ -77,12 +82,59 @@
 # inp = int(input("how many times do you want to print the message:"))
 # for i in range(inp):
 #     print("python is fun")
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-input_number = int(input("Enter a number to check if it's in the list: "))
-if input_number in list:
-    print(input_number, "is in the list")
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# def check_number_in_list():
+#     input_number = int(input("Enter a number to check if it's in the list: "))
+#     print(input_number in list)
+#     check_number_in_list()
+
+# check_number_in_list()
+
+
+# def find_factorial():
+#     num = int(input("Enter a number to find its factorial: "))
+#     factorial = 1
+#     for i in range(1, num + 1):
+#         factorial = factorial * i
+#         # factorial *= i
+#     print(f"The factorial of {num} is {factorial}")
+
+
+# def find_factorial(a):
+#     if a == 0 or a == 1:
+#         return 1
+#     else:
+#         return a * find_factorial(a-1)
     
-else:
-    print(input_number, "is not in the list")
+# print(find_factorial(5))
+
+# if input_number in list:
+#     print(input_number, "is in the list")
+#     int(input("Enter a number to check if it's in the list: "))
+# else:
+#     print(input_number, "is not in the list")
+#     int(input("Enter a number to check if it's in the list: "))
+
+# day 05
+
+# with open("new.txt", "x") as file:
+#     file.write("This is a new file created with the 'x' mode.\n")
+# with open("new.txt", "r") as file:
+#     content = file.read()
+# print(content)
+# print("File created and content read successfully.")
+
+# with open("new.txt", "a") as file:
+#     file.write("This is additional content added to the file.\n")
+#     file.write("This is another line of additional content.\n")
+#     file.write("End of additional content.\n")
+     
+
+# c = open("new.txt", "r")
+# for x in  c:
+#     print(x)  
     
-    
+# # print(content)
+# c.close()
+# print("Additional content added and file read successfully.")
